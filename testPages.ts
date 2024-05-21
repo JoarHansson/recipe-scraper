a3a632224981da2dfb7abdb8054477c15311c56c
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-05-21 16:03:22 +0200

    add URLs for testing recipe scraping

---
+++ testPages.ts
@@ -0,0 +1,25 @@
+export const urls: string[] = [
+  // WORKING EXAMPLES:
+  // version graph:
+  "https://www.halfbakedharvest.com/better-than-takeout-szechuan-noodles-with-sesame-chili-oil/",
+  "https://thewoksoflife.com/dan-dan-noodles/",
+  "https://lindseyeatsla.com/spicy-sichuan-noodles/",
+  "https://pinchofyum.com/lo-mein",
+  "https://www.aheadofthyme.com/shanghai-style-fried-noodles/",
+  "https://www.spoonforkbacon.com/garlic-noodles/",
+  // version root:
+  "https://www.ica.se/recept/pannkakor-grundsmet-2083/",
+  "https://www.onceuponachef.com/recipes/smash-burgers.html",
+
+  // NOT WORKING EXAMPLES:
+  // data not found / working but dataformat is slighly wrong:
+  // array wrapping the object
+  "https://www.koket.se/stekta-nudlar-filip-poons-recept",
+  // recipeInstructions is only a string
+  "https://www.scan.se/recept/605/wokade-kottbullar-med-sweet-chili-och-stekta-nudlar/",
+  // recipeInstructions is an array of objects, containing additional objects
+  "https://www.indianhealthyrecipes.com/veg-noodles-recipe/",
+  // recipeInstructions is an array with strings:
+  "https://www.bbc.co.uk/food/recipes/spicy_sichuan_noodles_79051",
+  "https://matinspo.se/recipe/kramiga-nudlar-med-wokade-gronsaker/",
+];
