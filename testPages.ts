3d6a2625f6efc957bf5a30f8ef8df78515a29637
Julia <jsgoransson@gmail.com>
2024-05-21 18:16:12 +0200

    added some more urls

--- testPages.ts
+++ testPages.ts
@@ -7,14 +7,21 @@ export const urls: string[] = [
   "https://pinchofyum.com/lo-mein",
   "https://www.aheadofthyme.com/shanghai-style-fried-noodles/",
   "https://www.spoonforkbacon.com/garlic-noodles/",
+  "https://natashaskitchen.com/perfect-burger-recipe/",
+  "https://www.aspicyperspective.com/best-hamburger-patty-recipe/",
   // version root:
   "https://www.ica.se/recept/pannkakor-grundsmet-2083/",
   "https://www.onceuponachef.com/recipes/smash-burgers.html",
+  "https://www.mathem.se/se/recipes/1896-mari-bergman-grillade-hamburgare-med-cheddarost-bacon-och-sotpo/?portions=4",
+  "https://realfood.tesco.com/recipes/classic-homemade-burger.html",
+  "https://www.hellofresh.se/recipes/saftig-blandfars-burgare-65faa270473c4f42771d1e34",
+  "https://www.food.com/recipe/the-perfect-burger-92021",
 
   // NOT WORKING EXAMPLES:
   // data not found / working but dataformat is slighly wrong:
   // array wrapping the object
   "https://www.koket.se/stekta-nudlar-filip-poons-recept",
+  "https://www.allrecipes.com/recipe/72657/best-hamburger-ever/",
   // recipeInstructions is only a string
   "https://www.scan.se/recept/605/wokade-kottbullar-med-sweet-chili-och-stekta-nudlar/",
   // recipeInstructions is an array of objects, containing additional objects
@@ -22,4 +29,7 @@ export const urls: string[] = [
   // recipeInstructions is an array with strings:
   "https://www.bbc.co.uk/food/recipes/spicy_sichuan_noodles_79051",
   "https://matinspo.se/recipe/kramiga-nudlar-med-wokade-gronsaker/",
+  "https://www.inspiredtaste.net/36554/veggie-burger-recipe/",
+  // Has the script tag, but no recipe data
+  "https://www.lyckasmedmat.se/blog/hamburgare-n269",
 ];
