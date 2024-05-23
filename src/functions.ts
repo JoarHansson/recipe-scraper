735d54ab5cf1ec001f246f38d319e51d783a5826
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-05-23 16:12:46 +0200

    add try/catch block and catch error
    catches error if there are no application/ld+json script containing "recipeIngredient" and "recipeInstructions"

--- src/functions.ts
+++ src/functions.ts
@@ -2,92 +2,97 @@ import he from "he";
 import { Browser, chromium, Page } from "playwright";
 
 export const getScrapedRecipe = async (url: string) => {
-  const browser: Browser = await chromium.launch({
-    headless: true,
-  });
-
-  const page: Page = await browser.newPage();
-  await page.goto(url);
-
-  // Get all scripts of type "application/ld+json" from the provided url
-  const jsonLdData: string[] = await page.evaluate(() => {
-    const scriptTags: NodeListOf<HTMLScriptElement> = document.querySelectorAll(
-      'script[type="application/ld+json"]'
-    );
-
-    const data: string[] = [];
-
-    scriptTags.forEach((scriptTag: HTMLScriptElement) => {
-      data.push(scriptTag.innerHTML);
+  try {
+    const browser: Browser = await chromium.launch({
+      headless: true,
     });
 
-    return data;
-  });
+    const page: Page = await browser.newPage();
+    await page.goto(url);
 
-  let scriptWithRecipeData: string = "";
+    // Get all scripts of type "application/ld+json" from the provided url
+    const jsonLdData: string[] = await page.evaluate(() => {
+      const scriptTags: NodeListOf<HTMLScriptElement> =
+        document.querySelectorAll('script[type="application/ld+json"]');
 
-  // Find the correct json string
-  jsonLdData.forEach((jsonString: string) => {
-    if (jsonString.includes("recipeIngredient")) {
-      scriptWithRecipeData = jsonString;
+      const data: string[] = [];
+
+      scriptTags.forEach((scriptTag: HTMLScriptElement) => {
+        data.push(scriptTag.innerHTML);
+      });
+
+      return data;
+    });
+
+    let scriptWithRecipeData: string = "";
+
+    // Find the correct json string
+    jsonLdData.forEach((jsonString: string) => {
+      if (
+        jsonString.includes("recipeIngredient") &&
+        jsonString.includes("recipeInstructions")
+      ) {
+        scriptWithRecipeData = jsonString;
+      }
+    });
+
+    if (!scriptWithRecipeData) {
+      throw new Error("no application/ld+json scripts");
     }
-  });
 
-  // Parse the correct json string
-  const parsed = JSON.parse(scriptWithRecipeData);
+    // Parse the correct json string
+    const parsed = JSON.parse(scriptWithRecipeData);
 
-  if (jsonLdData) {
-    console.log(true);
-  } else {
-    console.log(false);
+    // Check if desired data is on graph or root level
+    //On graph level
+    if (parsed["@graph"]) {
+      console.log("version graph");
+
+      const graph = parsed["@graph"];
+
+      type GraphObject = {
+        "@type": string | Array<any>;
+      };
+
+      let arrayKey: number = 0;
+
+      // Get the recipe object
+      graph.forEach((obj: GraphObject) => {
+        if (obj["@type"] === "Recipe") {
+          arrayKey = graph.indexOf(obj);
+        }
+      });
+
+      const ingredientsData = graph[arrayKey].recipeIngredient;
+      const instructionsData = graph[arrayKey].recipeInstructions;
+
+      // this logic can be re-used for recipeInstructions when we have it as a string[]
+      const parsedIngredients: string[] = ingredientsData.map(
+        (ingredient: string) => {
+          return he.decode(ingredient);
+        }
+      );
+
+      return parsedIngredients;
+    } // On root level
+    else if (parsed.recipeIngredient && parsed.recipeInstructions) {
+      console.log("version root");
+
+      // this logic can be re-used for recipeInstructions when we have it as a string[]
+      const parsedIngredients: string[] = parsed.recipeIngredient.map(
+        (ingredient: string) => {
+          return he.decode(ingredient);
+        }
+      );
+
+      return parsedIngredients;
+    } else {
+      console.log("data not found");
+    }
+
+    await browser.close();
+  } catch (error) {
+    console.error(error);
+    return error;
   }
-
-  // Check if desired data is on graph or root level
-  //On graph level
-  if (parsed["@graph"]) {
-    console.log("version graph");
-
-    const graph = parsed["@graph"];
-
-    type GraphObject = {
-      "@type": string | Array<any>;
-    };
-
-    let arrayKey: number = 0;
-
-    // Get the recipe object
-    graph.forEach((obj: GraphObject) => {
-      if (obj["@type"] === "Recipe") {
-        arrayKey = graph.indexOf(obj);
-      }
-    });
-
-    const ingredientsData = graph[arrayKey].recipeIngredient;
-    const instructionsData = graph[arrayKey].recipeInstructions;
-
-    // this logic can be re-used for recipeInstructions when we have it as a string[]
-    const parsedIngredients: string[] = ingredientsData.map(
-      (ingredient: string) => {
-        return he.decode(ingredient);
-      }
-    );
-
-    return parsedIngredients;
-  } // On root level
-  else if (parsed.recipeIngredient && parsed.recipeInstructions) {
-    console.log("version root");
-
-    // this logic can be re-used for recipeInstructions when we have it as a string[]
-    const parsedIngredients: string[] = parsed.recipeIngredient.map(
-      (ingredient: string) => {
-        return he.decode(ingredient);
-      }
-    );
-
-    return parsedIngredients;
-  } else {
-    console.log("data not found");
-  }
-
-  await browser.close();
 };
