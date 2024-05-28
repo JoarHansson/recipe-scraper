f9c8f4967a959ac9fee86dec483ca0dce948d514
Julia <jsgoransson@gmail.com>
2024-05-28 14:55:08 +0200

    added background to form

--- client/src/components/Form/Form.tsx
+++ client/src/components/Form/Form.tsx
@@ -1,10 +1,22 @@
 import styled from "@emotion/styled";
 
+const FormWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  align-items: center;
+`;
+
 const FormElement = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 1rem;
+  background-color: #f5f5f5;
+  border-radius: 1.5rem;
+  padding: 2rem 0;
+  max-width: 500px;
+  width: 90%;
 `;
 
 const InputWrapper = styled.div`
@@ -35,18 +47,20 @@ type Props = {
 
 export const Form = ({ url, handleInputChange, handleSubmit }: Props) => {
   return (
-    <FormElement onSubmit={handleSubmit}>
-      <InputWrapper>
-        <label htmlFor="urlInput">Paste your url here:</label>
-        <InputField
-          value={url || ""}
-          onChange={handleInputChange}
-          id="urlInput"
-          type="url"
-          placeholder="https://www.recipe.com"
-        />
-      </InputWrapper>
-      <button type="submit">Declutter</button>
-    </FormElement>
+    <FormWrapper>
+      <FormElement onSubmit={handleSubmit}>
+        <InputWrapper>
+          <label htmlFor="urlInput">Paste your url here:</label>
+          <InputField
+            value={url || ""}
+            onChange={handleInputChange}
+            id="urlInput"
+            type="url"
+            placeholder="https://www.recipe.com"
+          />
+        </InputWrapper>
+        <button type="submit">Declutter</button>
+      </FormElement>
+    </FormWrapper>
   );
 };
